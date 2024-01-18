2157e5c1b0d564221369d6fa26cb923e4d44e480
MauricioHS92 <hansenmailper@gmail.com>
2024-01-18 11:54:22 -0300

    new comments

--- RELOGIO-DIGITAL/script.js
+++ RELOGIO-DIGITAL/script.js
@@ -1,29 +1,29 @@
-function relogioHour() {
-    function getTime() {
-        const hour = new Date();
-        return hour.toLocaleTimeString('pt-BR', {
-            hour12: false
+function relogioHour() { // Função que inicia o relógio
+    function getTime() { // Função que pega a hora
+        const hour = new Date(); // Pega a hora atual
+        return hour.toLocaleTimeString('pt-BR', { // Formata a hora para o padrão brasileiro
+            hour12: false // Formata a hora para o padrão de 24 horas
         });
     }
 
-    const timer = setInterval(() => {
-        const display = document.querySelector('.display-hour');
-        display.innerHTML = getTime();
-    }, 1000);
+    const timer = setInterval(() => { // Atualiza o relógio a cada 1 segundo
+        const display = document.querySelector('.display-hour'); // Seleciona o elemento que vai receber a hora
+        display.innerHTML = getTime(); // Adiciona a hora no elemento selecionado
+    }, 1000); // Atualiza o relógio a cada 1 segundo
 }
 
-relogioHour();
+relogioHour(); // Inicia o relógio
 
-function formatarData(data) {
-    const diasSemana = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA'];
-    const mes = ('0' + (data.getMonth() + 1)).slice(-2);
-    const dia = ('0' + data.getDate()).slice(-2);
-    const diaSemana = diasSemana[data.getDay()];
+function formatarData(data) { // Formata a data para o padrão americano
+    const diasSemana = ['SU', 'MO', 'TU', 'WE', 'TH', 'FR', 'SA']; // Dias da semana em inglês
+    const mes = ('0' + (data.getMonth() + 1)).slice(-2); // Adiciona o 0 na frente do mês
+    const dia = ('0' + data.getDate()).slice(-2); // Adiciona o 0 na frente do dia
+    const diaSemana = diasSemana[data.getDay()]; // Pega o dia da semana
   
-    return `${diaSemana} ${mes}-${dia}`;
+    return `${diaSemana} ${mes}-${dia}`; // Retorna a data formatada
 }
 
-const dataAtual = new Date();
-const dataFormatada = formatarData(dataAtual);
-const display = document.querySelector('.display-date');
-display.innerHTML = dataFormatada;
+const dataAtual = new Date(); // Pega a data atual
+const dataFormatada = formatarData(dataAtual); // Formata a data atual
+const display = document.querySelector('.display-date'); // Seleciona o elemento que vai receber a data
+display.innerHTML = dataFormatada; // Adiciona a data formatada no elemento selecionado
